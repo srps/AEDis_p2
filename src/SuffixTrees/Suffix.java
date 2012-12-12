8279eb87b19c4286057b407f988d892414bf0fe8
Sérgio Miguel <a_k_a_bouncer@hotmail.com>
2012-12-12 00:02:58 +0000

    Construtor para classe Suffix que faltava

--- src/SuffixTrees/Suffix.java
+++ src/SuffixTrees/Suffix.java
@@ -7,5 +7,16 @@ package SuffixTrees;
  * Time: 21:17
  * To change this template use File | Settings | File Templates.
  */
-public class Suffix<I, S> {
+public class Suffix<I extends Character, S extends Iterable<I>> {
+
+    private int start;
+    private int end;
+    private String seq;
+
+    public Suffix(int start, int end, String sequence) {
+        this.start = start;
+        this.end = end;
+        this.seq = sequence;
+    }
+
 }

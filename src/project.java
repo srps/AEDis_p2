9f923fe37ddee0d0d2ea4033638b1c6b6ef892cd
Sérgio Miguel <a_k_a_bouncer@hotmail.com>
2012-12-13 00:05:55 +0000

    Mudanças no main. Ainda com bugs em multilinha.

--- src/project.java
+++ src/project.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class project {
@@ -9,43 +10,57 @@ public class project {
     public static void main(String[] args) {
 
         boolean finishedInput = false;
-        boolean finishedString = false;
+        boolean finishedSentence = false;
+        int stringCounter = 0;
         String[] input;
         String curLine;
         LinkedList<String> fifo = new LinkedList<>();
         StringBuilder builder = new StringBuilder();
+        Iterator<String> it;
 
         try (
                 InputStreamReader isr = new InputStreamReader(System.in);
                 BufferedReader in = new BufferedReader(isr)) {
 
             // While $$ sequence isn't seen
-            while (!finishedInput || !finishedString) {
+            while (!finishedInput || !finishedSentence || stringCounter > 0) {
                 // Checks if there's previously seen input to process
                 if (!fifo.isEmpty()) {
                     // Gets first unprocessed string and appends it to builder
                     builder.append(fifo.removeFirst());
+                    stringCounter--;
+                    finishedSentence = !fifo.isEmpty();
                     // Checks if there are potentially still unread characters in the current input
                     while (fifo.isEmpty()) {
                         // If so, go get them to process
                         curLine = in.readLine();
                         if (curLine.contains("$")) {
-                            finishedString = true;
+                            stringCounter += curLine.split("\\$").length;
+                            System.out.println("stringCounter: " + stringCounter);
                             finishedInput = curLine.contains("$$");
                             input = curLine.split("\\$");
                             fifo.addAll(Arrays.asList(input));
-
+                            it = fifo.iterator();
+                            while (it.hasNext()) {
+                                System.out.println(it.next());
+                            }
                         } else {
                             fifo.add(curLine);
                         }
                         if (!fifo.isEmpty()) {
                             builder.append(fifo.removeFirst());
+                            stringCounter--;
+                            finishedSentence = !fifo.isEmpty();
+                            break;
                         }
+                        System.out.println("current line: " + curLine);
+                        System.out.println(builder.toString());
                     }
                 } else {
                     curLine = in.readLine();
                     if (curLine.contains("$")) {
-                        finishedString = true;
+                        stringCounter += curLine.split("\\$").length;
+                        System.out.println("stringCounter: " + stringCounter);
                         finishedInput = curLine.contains("$$");
                         input = curLine.split("\\$");
                         fifo.addAll(Arrays.asList(input));
@@ -55,13 +70,14 @@ public class project {
                     }
                     if (!fifo.isEmpty()) {
                         builder.append(fifo.removeFirst());
+                        stringCounter--;
+                        finishedSentence = !fifo.isEmpty();
                     }
                 }
-                if (finishedString) {
+                if (finishedSentence) {
                     // Testing purposes only TODO : replace with SuffixTree call
                     System.out.println(builder.toString());
                     builder.setLength(0);
-                    finishedString = finishedInput;
                 }
             }
 
